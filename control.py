53c966d8a8261656562ad99aa692d8f0735fd248
mochamymocha <47485699+mochamymocha@users.noreply.github.com>
2023-09-12 16:30:58 -0400

    Functional Environment

--- control.py
+++ control.py
@@ -1,34 +1,44 @@
 import os
 from multiprocessing.connection import Listener, Client
 from PIL import Image
-import gym
+import gymnasium
 from matplotlib import pyplot as plt
 import numpy as np
+from stable_baselines3 import DQN, PPO
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.vec_env import SubprocVecEnv
+from stable_baselines3.common.logger import configure
+from datetime import datetime
 
-class GameEnv(gym.Env):
+class GameEnv(gymnasium.Env):
     def __init__(self):
+        with open('seed.txt', 'r') as file:
+            self.seed = int(file.read(1))
+        with open('seed.txt', 'w') as file:
+            file.write(str(self.seed+1))
         self.initialized = False
         self.listener_connection = None
         self.client_connection = None
         self.speed = 0.0
         self.position = 0.0
-        self.dolphin_path = "..\\dolphin\\Dolphin.exe"
+        self.dolphin_path = "..\\dolphin" + str(self.seed) + "\\Dolphin.exe"
         self.command = 'cmd /c ' + self.dolphin_path + " --script C:\\Users\\Carson\\MKAI\\Scripts\\dolphinscript.py \\b --exec C:\\Users\\Carson\\MKAI\\MKWii.iso"
         self.ydim = 90
         self.xdim = 168
-        self.observation_space = gym.spaces.Box(low = 0, high = 255, shape = (self.ydim, self.xdim), dtype=np.uint8)
-        self.action_space = gym.spaces.Discrete(6)
+        self.observation_space = gymnasium.spaces.Box(low = 0, high = 255, shape = (1, self.ydim, self.xdim), dtype=np.uint8)
+        self.action_space = gymnasium.spaces.Discrete(7)
+        self.initialize()
     
     def initialize(self):
         print('Starting Emulator')
         self.launch()
-        dolphinListener = ('localhost', 20000)
+        dolphinListener = ('localhost', 20000+self.seed)
         listener = Listener(dolphinListener, authkey=b'supersecure')
         print('Waiting...')
         self.listener_connection = listener.accept()
         msg = self.listener_connection.recv()
         print('Message received:', str(msg))
-        dolphinClient = ('localhost', 22000)
+        dolphinClient = ('localhost', 22000+self.seed)
         print('Connecting to script...')
         self.client_connection = Client(dolphinClient, authkey=b'password')
         self.client_connection.send('Testing!')
@@ -36,31 +46,39 @@ class GameEnv(gym.Env):
         self.reset()
     
     def step(self, action):
+        info2 = {}
         left = False
         right = False
-        a = False
+        sleft = False
+        sright = False
         up = False
         down = False
         l = False
         screen = False
         info = False
+        reward = 0
+        terminated = False
+        truncated = False
         if action == 0:
             left = True
         elif action == 1:
             right = True
         elif action == 2:
-            a = True
+            sleft = True
         elif action == 3:
-            up = True
+            sright = True
         elif action == 4:
-            down = True
+            up = True
         elif action == 5:
+            down = True
+        elif action == 6:
             l = True
         self.client_connection.send({
             'Type': 'Inputs',
             'StickLeft': left,
             'StickRight': right,
-            'A': a,
+            'SoftLeft': sleft,
+            'SoftRight': sright,
             'Up': up,
             'Down': down,
             'L': l
@@ -70,33 +88,53 @@ class GameEnv(gym.Env):
             if msg['Type'] == 'Screen' and not screen:
                 obs = np.asarray(msg['Data'])
                 obs = obs.astype(np.uint8)
+                obs = obs.reshape((1, 90, 168))
                 screen = True
             if msg['Type'] == 'Info' and not info:
-                info = {'Speed': msg['Speed'], 'Position': msg['Position'], 'Frames': msg['Frames']}
-                print(msg['Frames'])
+                info2 = {'Speed': msg['Speed'], 'Position': msg['Position'], 'Frames': msg['Frames']}
+                reward = msg['Reward']
+                # print(reward)
+                terminated = msg['Terminated']
+                truncated = msg['Truncated']
                 info = True
-        return (obs, info)
+        return (obs, reward, terminated, truncated, info2)
                 
 
     def launch(self):
         os.popen(self.command)
         print('Dolphin Loaded')
 
-    def reset(self, seed = None):
+    def reset(self, seed = None, options = None):
         super().reset(seed=seed)
+        print('resetting')
         self.client_connection.send({'Type': 'Reset'})
         while True:
             msg = self.listener_connection.recv()
             if msg['Type'] == 'Screen':
                 img = np.asarray(msg['Data'])
                 img = img.astype(np.uint8)
-                return img
+                img = img.reshape((1, 90, 168))
+                return img, {}
 
 def main():
-    test = GameEnv()
-    test.initialize()
+    with open('seed.txt', 'w') as file:
+            file.write(str(0))
+    env = make_vec_env(GameEnv, n_envs=4, seed=1, vec_env_cls=SubprocVecEnv)
+    model = PPO("MlpPolicy", env, verbose=1, tensorboard_log="./tbpp/")
+    model.learn(total_timesteps = 5000)
+    model.save("MKAIPPO")
+    with open('logcontrol.txt', 'w') as file:
+        file.write("Created new file!\n")
     while True:
-        test.step(2)
+        now = datetime.now()
+        time = now.strftime("%D-%H-%M-%S")
+        model.set_parameters("MKAIPPO", env)
+        model.learn(total_timesteps = 25000, reset_num_timesteps=False)
+        with open('logcontrol.txt', 'a') as file:
+            file.write('Done Iteration!\n')
+        model.save("MKAIPPO")
+        with open('logcontrol.txt', 'a') as file:
+            file.write('Saved Iteration!\n')
 
 if __name__ == '__main__':
     main()
--- dolphinscript.py
+++ dolphinscript.py
@@ -1,4 +1,4 @@
-from dolphin import event, memory, controller, savestate
+from dolphin import event, memory, controller, savestate, gui
 from multiprocessing.connection import Client, Listener
 import sys
 sys.path.append('C:\\Users\\Carson\\AppData\\Local\\Programs\\Python\\Python38\\Lib\\site-packages')
@@ -13,6 +13,8 @@ class Dolphin():
     def __init__(self):
         with open('log.txt', 'a') as file:
             file.write('New!\n')
+        with open('seed.txt', 'r') as file:
+            self.seed = int(file.read(1))
         self.input_map = {
             'StickLeft': False,
             'StickRight': False,
@@ -24,11 +26,40 @@ class Dolphin():
         self.speed = 0
         self.position = 0
         self.framecount = 0
-        self.prev_position = 0
+        self.recent_positions = []
         self.recent_speeds = []
-        self.client_connection = Client(('localhost', 20000), authkey=b'supersecure')
+        self.next_check = 1.05
+        self.total_reward = 0
+        done = False
+        try:
+            self.client_connection = Client(('localhost', 20000 + self.seed - 1), authkey=b'supersecure')
+            done = True
+            self.seed = self.seed - 1
+        except:
+            pass
+        if not done:
+            try:
+                self.client_connection = Client(('localhost', 20000 + self.seed - 2), authkey=b'supersecure')
+                done = True
+                self. seed = self.seed - 2
+            except:
+                pass
+        if not done:
+            try:
+                self.client_connection = Client(('localhost', 20000 + self.seed - 3), authkey=b'supersecure')
+                done = True
+                self.seed = self.seed - 3
+            except:
+                pass
+        if not done:
+            try:
+                self.client_connection = Client(('localhost', 20000 + self.seed - 4), authkey=b'supersecure')
+                done = True
+                self.seed = self.seed - 4
+            except:
+                pass
         self.client_connection.send("Script Started!")
-        self.listener = Listener(('localhost', 22000), authkey=b'password')
+        self.listener = Listener(('localhost', 22000 + self.seed), authkey=b'password')
         self.listener_connection = self.listener.accept()
         print('Waiting for response...')
         msg = self.listener_connection.recv()
@@ -39,18 +70,85 @@ class Dolphin():
     def update_env_reward(self):
         self.framecount += 1
         self.recent_speeds.append(self.speed)
-        if len(self.recent_speeds) >= 5:
+        if len(self.recent_speeds) >= 180:
             self.recent_speeds.pop(0)
-        self.speed = memory.read_f32(0x80FCE500)
-        print('speed: ' + str(self.speed))
-        self.prev_position =self.position
-        self.position = memory.read_f32(0x80E47870)
-        print('pos: ' + str(self.position))
+        self.speed = memory.read_f32(0x80FC6B6C)
+        self.recent_positions.append(self.position)
+        if len(self.recent_positions) >= 180:
+            self.recent_positions.pop(0)
+        self.position = memory.read_f32(0x80E418AC)
 
         reward = 0
         terminated = False
         truncated = False
 
+        if self.position > self.next_check:
+            self.next_check += 0.04
+            reward += 0.5 - min(0.25, self.framecount * 0.0025)
+            self.framecount = 0
+        # if self.position > self.recent_positions[-1]:
+        #     reward += 0.0025 * (self.position - self.recent_positions[-1])
+
+        if self.speed > 100:
+            reward += 0.008
+        elif self.speed > 95:
+            reward += 0.0055
+        elif self.speed > 90:
+            reward += 0.0035
+        elif self.speed > 85:
+            reward += 0.002
+        elif self.speed > 80:
+            reward += 0.001
+        elif self.speed > 75:
+            reward += 0.0005
+        elif self.speed > 70:
+            reward += 0.0001
+        elif self.speed < 35:
+            reward -= 0.01
+        elif self.speed < 45:
+            reward -= 0.0035
+        elif self.speed < 55:
+            reward -= 0.001
+        elif self.speed < 65:
+            reward -= 0.0001
+
+        # if self.speed > 100:
+        #     reward += 0.0065
+        # elif self.speed > 95:
+        #     reward += 0.004
+        # elif self.speed > 92:
+        #     reward += 0.003
+        # elif self.speed > 90:
+        #     reward += 0.0025
+        # elif self.speed > 87:
+        #     reward += 0.002
+        # elif self.speed > 85:
+        #     reward += 0.001
+        # elif self.speed > 82:
+        #     reward += 0.00075
+        # elif self.speed > 78:
+        #     reward += 0.0005
+        # elif self.speed > 70:
+        #     reward += 0.0001
+        # elif self.speed < 65:
+        #     reward -= 0.0001
+        # elif self.speed < 60:
+        #     reward -= 0.001
+        # elif self.speed < 50:
+        #     reward -= 0.005
+        # elif self.speed < 35:
+        #     reward -= 0.01
+
+        if self.position >= 4.0125:
+            reward += 9.75
+            terminated = True
+
+        elif (len(self.recent_speeds) == 179 and max(self.recent_speeds) < 45) or (len(self.recent_positions) == 179 and (self.recent_positions[-1]
+                                                                                                                         - self.recent_positions[0]) < 0.0025):
+            reward = -0.1
+            truncated = True
+        self.total_reward += reward
+        print(self.total_reward)
         return (reward, terminated, truncated)
 
 main = Dolphin()
@@ -68,15 +166,26 @@ while True:
     msg = main.listener_connection.recv()
     if msg['Type'] == 'Inputs':
         stickX = 128
-        if msg['StickLeft'] and not msg['StickRight']:
+        if msg['StickLeft']:
             stickX = 0
-        elif msg['StickRight'] and not msg['StickLeft']:
+        elif msg['StickRight']:
             stickX= 255
-        controller.set_gc_buttons(0, {'Left': False, 'Right': False, 'Down': msg['Down'], 'Up': msg['Up'], 'Z': False, 'R': False, 'L': msg['L'], 'A': msg['A'], 'B': False, 'X': False, 'Y': False, 'Start': False, 'StickX': stickX, 'StickY': 128, 'CStickX': 128, 'CStickY': 128, 'TriggerLeft': 0, 'TriggerRight': 0, 'AnalogA': 0, 'AnalogB': 0, 'Connected': True})
+        elif msg['SoftLeft']:
+            stickX = 64
+        elif msg['SoftRight']:
+            stickX = 192
+        controller.set_gc_buttons(0, {'Left': False, 'Right': False, 'Down': msg['Down'], 'Up': msg['Up'], 'Z': False, 'R': False, 'L': msg['L'], 'A': True, 'B': False, 'X': False, 'Y': False, 'Start': False, 'StickX': stickX, 'StickY': 128, 'CStickX': 128, 'CStickY': 128, 'TriggerLeft': 0, 'TriggerRight': 0, 'AnalogA': 0, 'AnalogB': 0, 'Connected': True})
     elif msg['Type'] == 'Reset':
         controller.set_gc_buttons(0, {'Left': False, 'Right': False, 'Down': False, 'Up': False, 'Z': False, 'R': False, 'L': False, 'A': False, 'B': False, 'X': False, 'Y': False, 'Start': False, 'StickX': 128, 'StickY': 128, 'CStickX': 128, 'CStickY': 128, 'TriggerLeft': 0, 'TriggerRight': 0, 'AnalogA': 0, 'AnalogB': 0, 'Connected': True})
+        main.recent_speeds = []
+        main.recent_positions = []
+        main.speed = 0
+        main.position = 0
+        main.framecount = 0
+        main.total_reward = 0
+        main.next_check = 1.05
         savestate.load_from_slot(1)
-    main.client_connection.send({'Type': 'Info', 'Speed': main.speed, 'Position': main.position, 'Frames': main.framecount})
+    main.client_connection.send({'Type': 'Info', 'Speed': main.speed, 'Position': main.position, 'Reward': reward, 'Frames': main.framecount, 'Terminated': terminated, 'Truncated': truncated})
     main.client_connection.send({'Type': 'Screen', 'Data': img, 'Width': 168, 'Height': 90})
     # print('Speed: ' + str(speed))
     # print('Track Position: ' + str(position))
